960277a2942386e766073dbe4cee536c98b49b7b
iuribe18 <ivanuribegonzalez@gmail.com>
2023-04-24 15:16:03 -0500

    modifying mysqli_connect

--- index.php
+++ index.php
@@ -104,7 +104,7 @@
             <div class="row it_works">
               <?php
 
-                        $link = mysqli_connect('terraform-20230422140750946000000007.ceilgdviwnhc.us-east-1.rds.amazonaws.com', 'ecomuser', 'CV4NAVGQSjl3r6SfaAvY', 'ecomdb');
+                        $link = mysqli_connect('terraform-20230424195606342000000001.ceilgdviwnhc.us-east-1.rds.amazonaws.com', 'ecomuser', 'CV4NAVGQSjl3r6SfaAvY', 'ecomdb');
 
                         if ($link) {
                         $res = mysqli_query($link, "select * from products;");
